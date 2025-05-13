7e90fecf30d39d2f5fb157682cb14d00e1041d92
Leonardo Vieira <jonaledi@gmail.com>
2025-05-13 12:30:16 -0300

    refactor: controller to work with the handler object

--- apps/subscription-server/src/controllers/dao.controller.ts
+++ apps/subscription-server/src/controllers/dao.controller.ts
@@ -2,22 +2,35 @@ import type { FastifyTypedInstance } from "../interfaces";
 import {
   subscriptionParamsSchema,
   subscriptionBodySchema,
-  subscriptionResponseSchema
+  createUpdateSubscriptionResponseSchema,
+  getDaoSubscribersResponseSchema
 } from '../schemas/subscription.schema';
-import { daoSubscriptionHandler } from '../handlers/dao.handlers';
+import { DaoHandler } from '../handlers/dao.handlers';
+import { userRepository, preferenceRepository } from '../index';
 
 /**
  * Registers DAO subscription routes in the Fastify app.
  * Acts as the controller layer, connecting HTTP routes to handlers.
  */
 export async function daoHandlers(app: FastifyTypedInstance) {
+  const daoHandler = new DaoHandler(userRepository, preferenceRepository);
+
   app.post('/subscription/:dao', {
     schema: {
       tags: ['dao'],
       description: 'Create or update dao subscription for user',
       params: subscriptionParamsSchema,
       body: subscriptionBodySchema,
-      response: subscriptionResponseSchema
+      response: createUpdateSubscriptionResponseSchema
     },
-  }, daoSubscriptionHandler);
+  }, (request) => daoHandler.postDaoSubscription(request));
+
+  app.get('/subscriptions/:dao', {
+    schema: {
+      tags: ['dao'],
+      description: 'Get all users subscribed to a specific DAO',
+      params: subscriptionParamsSchema,
+      response: getDaoSubscribersResponseSchema
+    }
+  }, (request) => daoHandler.getDaoSubscribers(request));
 } 
