3fe4e9f1cdafa60e4fc56dade826a337838066ae
Leonardo Vieira <jonaledi@gmail.com>
2025-05-13 11:10:41 -0300

    refactor: config file to be ts

---
+++ apps/subscription-server/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.json',
+      isolatedModules: true
+    }]
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'
+};
+
+export default config; 
