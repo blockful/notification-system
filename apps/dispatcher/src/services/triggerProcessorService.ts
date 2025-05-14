47aafe54d82424bcd6f2cfe528a1374d94483e4d
Leonardo Vieira <jonaledi@gmail.com>
2025-05-14 13:40:42 -0300

    add: trigger router

---
+++ apps/dispatcher/src/services/triggerProcessorService.ts
@@ -0,0 +1,28 @@
+import { DispatcherMessage, MessageProcessingResult } from "../interfaces/dispatcher_message";
+import { TriggerHandler } from "./triggers/triggerHandler";
+import { NewProposalTriggerHandler } from "./triggers/newProposalHandler";
+
+/**
+ * Service responsible for processing messages for specific triggers
+ */
+export class TriggerProcessorService {
+  private triggerHandlers: Map<string, TriggerHandler>;
+  constructor() {
+    this.triggerHandlers = new Map();
+    this.triggerHandlers.set('new-proposal', new NewProposalTriggerHandler());
+  }
+
+  /**
+   * Process a message for a specific trigger
+   * @param message The message to process
+   * @returns Processing result
+   * @throws Error if trigger handler not found
+   */
+  async processTrigger(message: DispatcherMessage): Promise<MessageProcessingResult> {
+    const handler = this.triggerHandlers.get(message.triggerId);
+    if (!handler) {
+      throw new Error(`No handler registered for trigger: ${message.triggerId}`);
+    }
+    return await handler.handleMessage(message);
+  }
+} 
