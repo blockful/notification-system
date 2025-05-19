20ca5005390d116d0fa93dd6f2b1269bbd94a94a
Leonardo Vieira <jonaledi@gmail.com>
2025-05-19 08:28:43 -0300

    add: swc/jest to fix trispilation problem

--- apps/logic-system/jest.config.ts
+++ apps/logic-system/jest.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.ts'],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
+  }
 };
 
 export default config; 
