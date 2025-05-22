fbac757a6072386824e10e87a92301a71b5c8cf4
Leonardo Vieira <jonaledi@gmail.com>
2025-05-22 11:37:53 -0300

    add: dotenv.config

--- apps/dispatcher/src/envConfig.ts
+++ apps/dispatcher/src/envConfig.ts
@@ -4,8 +4,10 @@
  * 
  */
 
+import dotenv from 'dotenv';
 import { z } from 'zod';
 
+dotenv.config();
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   PORT: z.coerce.number().default(3000),
