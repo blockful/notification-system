e7497972de876ec2eb30434f11ffa8a8a75f7e12
Leonardo Vieira <jonaledi@gmail.com>
2025-05-23 14:09:16 -0300

    refactor: logic-system to split index responsabilities

---
+++ apps/logic-system/src/app.ts
@@ -0,0 +1,33 @@
+import { NewProposalTrigger } from './triggers/new-proposal-trigger';
+import { ProposalRepository } from './repositories/proposal.repository';
+import { DispatcherApiClient } from './api-clients/dispatcher.api-client';
+import { ProposalStatus } from './interfaces/proposal.interface';
+import { Knex } from 'knex';
+
+export class App {
+  private trigger: NewProposalTrigger;
+  private proposalStatus: ProposalStatus;
+
+  constructor(db: Knex, dispatcherEndpoint: string, triggerInterval: number, proposalStatus: ProposalStatus) {
+    this.proposalStatus = proposalStatus;
+    
+    const proposalDB = new ProposalRepository(db);
+    const dispatcherService = new DispatcherApiClient(dispatcherEndpoint);
+
+    this.trigger = new NewProposalTrigger(
+      dispatcherService,
+      proposalDB,
+      triggerInterval
+    );
+  }
+
+  start(): void {
+    this.trigger.start({ status: this.proposalStatus });
+    console.log('Logic system is running. Press Ctrl+C to stop.');
+  }
+}
+
+//@ts-ignore
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+}; 
