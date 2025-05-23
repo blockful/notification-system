4fecb01d1c23d8b30816d9d5fdc514d94adeec9d
Leonardo Vieira <jonaledi@gmail.com>
2025-05-23 08:27:23 -0300

    refactor: way of sharing modules in the monorepo

--- apps/integrated-tests/jest.config.ts
+++ apps/integrated-tests/jest.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
 import path from 'path';
 
 const config: Config = {
@@ -7,10 +6,6 @@ const config: Config = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testTimeout: 30000,
-  moduleNameMapper: {
-    '^@notification-system/(.*)$': path.resolve(__dirname, '../$1')
-  },
-  moduleDirectories: ['node_modules', path.resolve(__dirname, '../..')],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: path.resolve(__dirname, 'tsconfig.json')
