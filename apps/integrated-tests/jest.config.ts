ae0237adffae604bae8faa7c4c052bd55d62bd1d
Leonardo Vieira <jonaledi@gmail.com>
2025-05-27 10:51:55 -0300

    refactor: jest config to remove unnecessary configs

--- apps/integrated-tests/jest.config.ts
+++ apps/integrated-tests/jest.config.ts
@@ -1,16 +1,12 @@
-import type { Config } from 'jest';
-import path from 'path';
+import { pathsToModuleNameMapper } from 'ts-jest';
+const { compilerOptions } = require('../../tsconfig.json');
 
-const config: Config = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testTimeout: 30000,
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: path.resolve(__dirname, 'tsconfig.json')
-    }]
-  }
-};
-
-export default config; 
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { 
+    prefix: '<rootDir>/../../' 
+  })
+}; 
