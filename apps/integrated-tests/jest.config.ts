6674b5eeb0f1a1e0918081c5e0062db8410e8436
Leonardo Vieira <jonaledi@gmail.com>
2025-05-28 10:05:56 -0300

    remove: moduleNameMapper in the jest.config

--- apps/integrated-tests/jest.config.ts
+++ apps/integrated-tests/jest.config.ts
@@ -1,12 +1,6 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-const { compilerOptions } = require('../../tsconfig.json');
-
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testTimeout: 30000,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { 
-    prefix: '<rootDir>/../../' 
-  })
+  testTimeout: 30000
 }; 
