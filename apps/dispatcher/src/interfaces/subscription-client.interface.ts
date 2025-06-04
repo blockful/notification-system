12de3238badd7f428d33e12dcb8e1fb6d14d53f8
Leonardo Vieira <jonaledi@gmail.com>
2025-06-04 11:17:13 -0300

    add: subscription service interface

--- apps/dispatcher/src/interfaces/subscription-client.interface.ts
+++ apps/dispatcher/src/interfaces/subscription-client.interface.ts
@@ -10,6 +10,15 @@ export interface User {
   }
 
 /**
+ * Interface defining a notification for deduplication
+ */
+export interface Notification {
+  user_id: string;
+  proposal_id: string;
+  dao_id: string;
+}
+
+/**
  * Interface for the Subscription Server client
  * Defines the contract for communication with the subscription service
  */
@@ -20,4 +29,20 @@ export interface ISubscriptionClient {
    * @returns List of subscribers
    */
   getDaoSubscribers(daoId: string): Promise<User[]>;
+
+  /**
+   * Filters subscribers to return only those who should receive notifications
+   * @param subscribers List of subscribers to check
+   * @param proposalId The proposal ID
+   * @param daoId The DAO ID
+   * @returns Filtered list of subscribers that should receive notifications
+   */
+  shouldSend(subscribers: User[], proposalId: string, daoId: string): Promise<Notification[]>;
+
+  /**
+   * Marks notifications as sent for successful deliveries
+   * @param notifications List of notifications to mark as sent
+   * @returns Number of notifications marked as sent
+   */
+  markAsSent(notifications: Notification[]): Promise<number>;
 } 
