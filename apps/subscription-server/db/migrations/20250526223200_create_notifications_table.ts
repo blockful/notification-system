0f8ea3137d285eda9fb400f7e9a690f89fc21ed2
Leonardo Vieira <jonaledi@gmail.com>
2025-06-06 16:47:33 -0300

    refactor: migrations

--- apps/subscription-server/db/migrations/20250526223200_create_notifications_table.ts
+++ apps/subscription-server/db/migrations/20250526223200_create_notifications_table.ts
@@ -4,9 +4,9 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('notifications', (table) => {
     table.string('user_id').notNullable();
     table.string('dao_id').notNullable();
-    table.string('proposal_id').notNullable();
+    table.string('event_id').notNullable();
     table.datetime('created_at').notNullable().defaultTo(knex.fn.now());
-    table.primary(['user_id', 'dao_id', 'proposal_id']);
+    table.primary(['user_id', 'dao_id', 'event_id']);
   });
 }
 
