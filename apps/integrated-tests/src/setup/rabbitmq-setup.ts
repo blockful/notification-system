9909f177735cd188b4745345d250427d4610ca0e
Leonardo Vieira <jonaledi@gmail.com>
2025-06-25 14:49:22 -0300

    add: rabbitmql setup file for tests

---
+++ apps/integrated-tests/src/setup/rabbitmq-setup.ts
@@ -0,0 +1,39 @@
+import { RabbitMQContainer, StartedRabbitMQContainer } from '@testcontainers/rabbitmq';
+import { RabbitMQConnection } from '@notification-system/rabbitmq-client';
+
+export class RabbitMQTestSetup {
+  private container: StartedRabbitMQContainer | null = null;
+  private connection: RabbitMQConnection | null = null;
+  
+  async setup(): Promise<string> {
+    this.container = await new RabbitMQContainer().start();
+    const amqpUrl = this.container.getAmqpUrl();
+    this.connection = new RabbitMQConnection(amqpUrl);
+    await this.connection.connect();
+    await this.clearQueue('dispatcher-queue');
+
+    return amqpUrl;
+  }
+
+  async cleanup(): Promise<void> {
+    if (this.connection) {
+      await this.clearQueue('dispatcher-queue');
+      await this.connection.close();
+      this.connection = null;
+    }
+    
+    if (this.container) {
+      await this.container.stop();
+      this.container = null;
+    }
+  }
+
+  private async clearQueue(queueName: string): Promise<void> {
+    if (!this.connection) return;
+    
+    const channel = await this.connection.createChannel();
+    await channel.assertQueue(queueName, { durable: true });
+    await channel.purgeQueue(queueName);
+    await channel.close();
+  }
+}
