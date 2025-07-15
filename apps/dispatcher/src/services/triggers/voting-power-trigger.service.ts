c47fdac9af27bbf7e2f0d17d4dd05ece2624763c
Leonardo Vieira <jonaledi@gmail.com>
2025-07-15 15:34:05 -0300

    refactor: votingpower notificationMessage

--- apps/dispatcher/src/services/triggers/voting-power-trigger.service.ts
+++ apps/dispatcher/src/services/triggers/voting-power-trigger.service.ts
@@ -65,24 +65,20 @@ export class VotingPowerTriggerHandler extends BaseTriggerHandler {
 
       if (changeType === 'delegation') {
         // Delegation event - sourceAccountId is delegatorAccountId
-        if (deltaValue > 0) {
+        if (deltaValue >= 0) {
           notificationMessage = `🥳 You've received a new delegation in ${daoId}!\n${sourceAccountId} delegated to you, increasing your voting power by ${deltaValue}.`;
         } else if (deltaValue < 0) {
           notificationMessage = `🥺 A delegator just undelegated in ${daoId}!\n${sourceAccountId} removed their delegation, reducing your voting power by ${deltaValue}.`;
-        } else {
-          notificationMessage = `🔄 Delegation activity detected in ${daoId}!\n${sourceAccountId} modified their delegation to you.`;
-        }
+        } 
       } else if (changeType === 'transfer') {
         // Transfer event - check who caused the change based on delta
-        if (deltaValue > 0) {
+        if (deltaValue >= 0) {
           // Received tokens - sourceAccountId is fromAccountId (who sent)
           notificationMessage = `💰 You've received new tokens in ${daoId}!\n${sourceAccountId} transferred tokens to you, increasing your voting power by ${deltaValue}.`;
         } else if (deltaValue < 0) {
           // Sent tokens - targetAccountId is toAccountId (who received)  
           notificationMessage = `📤 You've transferred tokens in ${daoId}!\nYou sent tokens to ${targetAccountId}, reducing your voting power by ${deltaValue}.`;
-        } else {
-          notificationMessage = `🔄 Token transfer activity detected in ${daoId}!\nTransfer completed with no voting power change.`;
-        }
+        } 
       } else {
         // Generic voting power change
         if (deltaValue !== 0) {
