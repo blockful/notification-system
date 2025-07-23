67245c6f8c1a3a6b40d748e6dacec5c889a52858
Leonardo Vieira <jonaledi@gmail.com>
2025-07-23 13:09:51 -0300

    add: jest setup

---
+++ apps/integrated-tests/src/setup/jest-global-setup.ts
@@ -0,0 +1,22 @@
+import { RabbitMQContainer, StartedRabbitMQContainer } from '@testcontainers/rabbitmq';
+
+// Global state to share RabbitMQ URL
+declare global {
+  var __RABBITMQ_URL__: string;
+  var __RABBITMQ_CONTAINER__: StartedRabbitMQContainer;
+}
+
+export default async function globalSetup() {
+  const container = await new RabbitMQContainer()
+    .withStartupTimeout(150000)
+    .start();
+  
+  const amqpUrl = container.getAmqpUrl();
+  
+  // Store RabbitMQ URL in environment variable for tests to access
+  process.env.TEST_RABBITMQ_URL = amqpUrl;
+  
+  // Store container reference for teardown
+  global.__RABBITMQ_CONTAINER__ = container;
+  global.__RABBITMQ_URL__ = amqpUrl;
+}
