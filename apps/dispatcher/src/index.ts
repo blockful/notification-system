35a1199cc6052648ec502b91d4db236f1034028c
Leonardo Vieira <jonaledi@gmail.com>
2025-08-26 18:05:24 -0300

    add: insert anticaptureGraphqlEndpoint on app initialization

--- apps/dispatcher/src/index.ts
+++ apps/dispatcher/src/index.ts
@@ -2,7 +2,11 @@ import { App } from './app';
 import { loadConfig } from './envConfig';
 
 const config = loadConfig();
-const app = new App(config.subscriptionServerUrl, config.rabbitmqUrl);
+const app = new App(
+  config.subscriptionServerUrl, 
+  config.rabbitmqUrl, 
+  config.anticaptureGraphqlEndpoint
+);
 
 (async () => {
   await app.start();
