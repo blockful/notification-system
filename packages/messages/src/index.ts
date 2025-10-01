1b2fce68e8a62a49d402837bfa934ae98dc544b3
Leonardo Vieira <jonaledi@gmail.com>
2025-10-01 16:33:34 -0300

    refactor: exports

--- packages/messages/src/index.ts
+++ packages/messages/src/index.ts
@@ -3,20 +3,20 @@
  */
 
 // Export trigger messages
-export { newProposalMessages } from './triggers/new-proposal';
-export { voteConfirmationMessages } from './triggers/vote-confirmation';
-export { votingReminderMessages } from './triggers/voting-reminder';
-export { proposalFinishedMessages } from './triggers/proposal-finished';
-export { votingPowerMessages } from './triggers/voting-power';
-export { nonVotingMessages } from './triggers/non-voting';
-export { delegationChangeMessages } from './triggers/delegation-change';
+export * from './triggers/new-proposal';
+export * from './triggers/vote-confirmation';
+export * from './triggers/voting-reminder';
+export * from './triggers/proposal-finished';
+export * from './triggers/voting-power';
+export * from './triggers/non-voting';
+export * from './triggers/delegation-change';
 
 // Export UI messages
-export { uiMessages } from './ui/common';
-export { slackMessages } from './ui/slack';
-export { telegramMessages } from './ui/telegram';
+export * from './ui/common';
+export * from './ui/slack';
+export * from './ui/telegram';
 
 // Export formatters and utilities
-export { replacePlaceholders } from './formatters/placeholders';
-export type { PlaceholderMap } from './formatters/placeholders';
-export { getDaoWithEmoji } from './formatters/dao-emoji';
+export * from './formatters/placeholders';
+export type * from './formatters/placeholders';
+export * from './formatters/dao-emoji';
