21aee2baa7b12e861c1594735efc7ffb2ee082a1
Leonardo Vieira <jonaledi@gmail.com>
2026-02-26 12:57:50 -0300

    feat: MSW lib

--- packages/anticapture-client/jest.config.js
+++ packages/anticapture-client/jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/.pnpm/(?!(msw|@mswjs|until-async|@bundled-es-modules)[@+])',
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': ['ts-jest', { tsconfig: { allowJs: true } }],
+  },
   collectCoverageFrom: [
     'src/**/*.{ts,js}',
     '!src/**/*.d.ts',
